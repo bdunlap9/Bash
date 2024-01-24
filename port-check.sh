ffc0146cdf445b89716550d3bacc8fa2d4f48a16
Weeke <baileydunlap9@gmail.com>
2024-01-24 14:04:18 -0500

    Update port-check.sh

--- port-check.sh
+++ port-check.sh
@@ -1,15 +1,16 @@
 function check_all_ports {
     local host=$1
+    local output_file="open_ports.txt"
 
     for port in {1..65535}; do
         if nc -zv -w1 "$host" "$port" <<< '' 2>&1 | grep -q 'succeeded'; then
             echo "[*] Port $port is open"
+            echo "[*] Port $port is open" >> "$output_file"
         elif nc -zv -w1 "$host" "$port" <<< '' 2>&1 | grep -q 'refused'; then
             echo "[*] Port $port is closed (connection refused)"
         else
-            echo ""
+            echo "[*] Port $port status is unknown"
         fi
     done
 }
-
 check_all_ports "localhost"
